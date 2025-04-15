e2ee58ed7b91a62f144faa55994566d643454879
Maxim Dunaitsev <maksim.dunaitsev@gmail.com>
2025-04-15 05:41:55 +0500

    feat: add basic server metrics

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"expvar"
 	"fmt"
 	"net/http"
 	"os"
@@ -112,6 +113,7 @@ func (app *application) mount() http.Handler {
 	r.Route("/v1", func(r chi.Router) {
 		// r.With(app.BasicAuthMiddleware).Get("/health", app.healthCheckHandler)
 		r.Get("/health", app.healthCheckHandler)
+		r.With(app.BasicAuthMiddleware).Get("/metrics", expvar.Handler().ServeHTTP)
 
 		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.addr)
 		r.Get("/swagger/*", httpSwagger.Handler(
